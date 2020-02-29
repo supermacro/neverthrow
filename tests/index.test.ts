979b3c1068fea7fc6a1b46df234f10eb2443275c
Giorgio Delgado <hi@gdelgado.ca>
2020-02-29 15:26:46 -0500

    Format tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,9 +2,8 @@ import { ok, err, Ok, Err, Result } from '../src'
 
 describe('Result.Ok', () => {
   it('Creates an Ok value', () => {
-    
     const okVal = ok(12)
-    
+
     expect(okVal.isOk()).toBe(true)
     expect(okVal.isErr()).toBe(false)
     expect(okVal).toBeInstanceOf(Ok)
@@ -38,11 +37,9 @@ describe('Result.Ok', () => {
   })
 
   it('Skips `mapErr`', () => {
-    const mapErrorFunc = jest.fn((_error) => 'mapped error value')
+    const mapErrorFunc = jest.fn(_error => 'mapped error value')
 
-    const notMapped = ok(12).mapErr(
-      mapErrorFunc
-    )
+    const notMapped = ok(12).mapErr(mapErrorFunc)
 
     expect(notMapped.isOk()).toBe(true)
     expect(mapErrorFunc).not.toHaveBeenCalledTimes(1)
@@ -51,31 +48,31 @@ describe('Result.Ok', () => {
   describe('andThen', () => {
     it('Maps to an Ok', () => {
       const okVal = ok(12)
-    
-      const flattened = okVal.andThen((_number) => {
+
+      const flattened = okVal.andThen(_number => {
         // ...
         // complex logic
-        // ... 
+        // ...
         return ok({ data: 'why not' })
       })
-    
+
       expect(flattened.isOk()).toBe(true)
       expect(flattened._unsafeUnwrap()).toStrictEqual({ data: 'why not' })
     })
 
     it('Maps to an Err', () => {
       const okval = ok(12)
-  
-      const flattened = okval.andThen((_number) => {
+
+      const flattened = okval.andThen(_number => {
         // ...
         // complex logic
-        // ... 
+        // ...
         return err('Whoopsies!')
       })
-  
+
       expect(flattened.isOk()).toBe(false)
 
-      const nextFn = jest.fn((_val) => ok('noop'))
+      const nextFn = jest.fn(_val => ok('noop'))
 
       flattened.andThen(nextFn)
 
@@ -83,9 +80,8 @@ describe('Result.Ok', () => {
     })
   })
 
-
   it('Maps to a promise', async () => {
-    const asyncMapper = jest.fn((_val) => {
+    const asyncMapper = jest.fn(_val => {
       // ...
       // complex logic
       // ..
@@ -94,9 +90,7 @@ describe('Result.Ok', () => {
       // network calls
       // disk io
       // etc ...
-      return Promise.resolve(
-        ok('Very Nice!')
-      )
+      return Promise.resolve(ok('Very Nice!'))
     })
 
     const okVal = ok(12)
@@ -112,13 +106,10 @@ describe('Result.Ok', () => {
   })
 
   it('Matches on an Ok', () => {
-    const okMapper = jest.fn((_val) => 'weeeeee')
-    const errMapper = jest.fn((_val) => 'wooooo')
+    const okMapper = jest.fn(_val => 'weeeeee')
+    const errMapper = jest.fn(_val => 'wooooo')
 
-    const matched = ok(12).match(
-      okMapper,
-      errMapper
-    )
+    const matched = ok(12).match(okMapper, errMapper)
 
     expect(matched).toBe('weeeeee')
     expect(okMapper).toHaveBeenCalledTimes(1)
@@ -132,21 +123,21 @@ describe('Result.Ok', () => {
   })
 
   it('Can read the value after narrowing', () => {
-    const fallible: () => Result<string, number> = () => ok('safe to read');
-    const val = fallible();
+    const fallible: () => Result<string, number> = () => ok('safe to read')
+    const val = fallible()
 
     // After this check we val is narrowed to Ok<string, number>. Without this
     // line TypeScript will not allow accessing val.value.
-    if (val.isErr()) return;
+    if (val.isErr()) return
 
-    expect(val.value).toBe('safe to read');
+    expect(val.value).toBe('safe to read')
   })
 })
 
 describe('Result.Err', () => {
   it('Creates an Err value', () => {
     const errVal = err('I have you now.')
-    
+
     expect(errVal.isOk()).toBe(false)
     expect(errVal.isErr()).toBe(true)
     expect(errVal).toBeInstanceOf(Err)
@@ -155,7 +146,7 @@ describe('Result.Err', () => {
   it('Skips `map`', () => {
     const errVal = err('I am your father')
 
-    const mapper = jest.fn((_value) => 'noooo')
+    const mapper = jest.fn(_value => 'noooo')
 
     const hopefullyNotMapped = errVal.map(mapper)
 
@@ -167,9 +158,7 @@ describe('Result.Err', () => {
   it('Maps over an Err', () => {
     const errVal = err('Round 1, Fight!')
 
-    const mapper = jest.fn(
-      (error: string) => error.replace('1', '2')
-    )
+    const mapper = jest.fn((error: string) => error.replace('1', '2'))
 
     const mapped = errVal.mapErr(mapper)
 
@@ -181,9 +170,7 @@ describe('Result.Err', () => {
   it('Skips over andThen', () => {
     const errVal = err('Yolo')
 
-    const mapper = jest.fn(
-      (_val) => ok<string, string>('yooyo')
-    )
+    const mapper = jest.fn(_val => ok<string, string>('yooyo'))
 
     const hopefullyNotFlattened = errVal.andThen(mapper)
 
@@ -193,7 +180,7 @@ describe('Result.Err', () => {
   })
 
   it('Does not invoke callback within `asyncMap`', async () => {
-    const asyncMapper = jest.fn((_val) => {
+    const asyncMapper = jest.fn(_val => {
       // ...
       // complex logic
       // ..
@@ -202,9 +189,7 @@ describe('Result.Err', () => {
       // network calls
       // disk io
       // etc ...
-      return Promise.resolve(
-        ok('Very Nice!')
-      )
+      return Promise.resolve(ok('Very Nice!'))
     })
 
     const errVal = err('nooooooo')
@@ -221,13 +206,10 @@ describe('Result.Err', () => {
   })
 
   it('Matches on an Err', () => {
-    const okMapper = jest.fn((_val) => 'weeeeee')
-    const errMapper = jest.fn((_val) => 'wooooo')
+    const okMapper = jest.fn(_val => 'weeeeee')
+    const errMapper = jest.fn(_val => 'wooooo')
 
-    const matched = err(12).match(
-      okMapper,
-      errMapper
-    )
+    const matched = err(12).match(okMapper, errMapper)
 
     expect(matched).toBe('wooooo')
     expect(okMapper).not.toHaveBeenCalled()
@@ -245,6 +227,6 @@ describe('Result.Err', () => {
   it('Unwraps without issue', () => {
     const okVal = err(12)
 
-    expect(okVal._unsafeUnwrapErr()).toBe(12)    
+    expect(okVal._unsafeUnwrapErr()).toBe(12)
   })
 })
