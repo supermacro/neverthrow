3dd1146c3755034e5c47b87d530b1740714fff40
Pierre Duchateau <pad@dabla.com>
2020-05-18 12:17:53 +0200

    Refactor ResultAsync methods to avoid unnecessary instantiation

--- src/result-async.ts
+++ src/result-async.ts
@@ -27,52 +27,45 @@ export class ResultAsync<T, E> {
     return new ResultAsync(newPromise)
   }
 
-  map<A>(f: (t: T) => A | Promise<A>): ResultAsync<A, E> {
-    const newPromise = this._promise.then(async (res: Result<T, E>) => {
-      if (res.isErr()) {
-        return new Err<A, E>(res.error)
-      }
+  map = <A>(f: (t: T) => A | Promise<A>) =>
+    new ResultAsync(
+      this._promise.then(async (res: Result<T, E>) => {
+        if (res.isErr()) {
+          return new Err<A, E>(res.error)
+        }
 
-      return new Ok<A, E>(await f(res.value))
-    })
+        return new Ok<A, E>(await f(res.value))
+      }),
+    )
 
-    return new ResultAsync(newPromise)
-  }
+  mapErr = <U>(f: (e: E) => U | Promise<U>) =>
+    new ResultAsync(
+      this._promise.then(async (res: Result<T, E>) => {
+        if (res.isOk()) {
+          return new Ok<T, U>(res.value)
+        }
 
-  mapErr<U>(f: (e: E) => U | Promise<U>): ResultAsync<T, U> {
-    const newPromise = this._promise.then(async (res: Result<T, E>) => {
-      if (res.isOk()) {
-        return new Ok<T, U>(res.value)
-      }
+        return new Err<T, U>(await f(res.error))
+      }),
+    )
 
-      return new Err<T, U>(await f(res.error))
-    })
+  andThen = <U>(f: (t: T) => Result<U, E> | ResultAsync<U, E>) =>
+    new ResultAsync(
+      this._promise.then(res => {
+        if (res.isErr()) {
+          return new Err<U, E>(res.error)
+        }
 
-    return new ResultAsync(newPromise)
-  }
+        const newValue = f(res.value)
 
-  andThen<U>(f: (t: T) => Result<U, E> | ResultAsync<U, E>): ResultAsync<U, E> {
-    const newPromise = this._promise.then(res => {
-      if (res.isErr()) {
-        return new Err<U, E>(res.error)
-      }
+        return newValue instanceof ResultAsync ? newValue._promise : newValue
+      }),
+    )
 
-      const newValue = f(res.value)
-
-      return newValue instanceof ResultAsync ? newValue._promise : newValue
-    })
-
-    return new ResultAsync(newPromise)
-  }
-
-  match = <A>(ok: (t: T) => A, _err: (e: E) => A): Promise<A> => {
-    return this._promise.then(res => res.match(ok, _err))
-  }
+  match = <A>(ok: (t: T) => A, _err: (e: E) => A) => this._promise.then(res => res.match(ok, _err))
 
   // Makes ResultAsync awaitable
-  then<A>(successCallback: (res: Result<T, E>) => A): Promise<A> {
-    return this._promise.then(successCallback)
-  }
+  then = <A>(successCallback: (res: Result<T, E>) => A) => this._promise.then(successCallback)
 }
 
 export const okAsync = <T, E>(value: T): ResultAsync<T, E> =>
