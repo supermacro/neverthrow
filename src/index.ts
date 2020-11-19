08242791c49142eeb5b8dd69b111a8719589eab3
Giorgio Delgado <hi@gdelgado.ca>
2020-11-19 18:51:49 -0500

    Remove chain api entirely

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { Result, ok, Ok, err, Err } from './result'
 export { ResultAsync, okAsync, errAsync } from './result-async'
-export { chain, chain3, chain4, chain5, chain6, chain7, chain8 } from './chain'
 export { combine } from './utils'
