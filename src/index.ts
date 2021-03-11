9ae007ae446c6b48fbcf8de623cc936ce62c2b1f
Tobias Løfgren <tobloef@gmail.com>
2021-03-11 20:42:22 +0100

    Fix combineWithAllErrors not being properly exported

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Result, ok, Ok, err, Err, fromThrowable } from './result'
 export { ResultAsync, okAsync, errAsync, fromPromise, fromSafePromise } from './result-async'
-export { combine } from './utils'
+export { combine, combineWithAllErrors } from './utils'
