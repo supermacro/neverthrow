04f5f5b17030469c9314587197e2989be9fccbc5
Giorgio Delgado <hi@gdelgado.ca>
2021-10-18 13:37:59 +0200

    Fix: remove extra line in result-async.ts

--- src/result-async.ts
+++ src/result-async.ts
@@ -46,7 +46,6 @@ export class ResultAsync<T, E> implements PromiseLike<Result<T, E>> {
     )
   }
 
-
   andThen<R extends Result<unknown, unknown>>(
     f: (t: T) => R,
   ): ResultAsync<InferOkTypes<R>, InferErrTypes<R> | E>
