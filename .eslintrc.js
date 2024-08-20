fb701ed56db77ef10b35edf29f3ef5b90a295565
Matt Pocock <mattpocockvoice@gmail.com>
2024-08-20 10:35:50 +0100

    Prettier and eslint improvements (no upgrades yet)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,11 @@ module.exports = {
     node: true,
   },
   ignorePatterns: ['dist/', 'tests/'],
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
   ],
   rules: {
     semi: 'off',
@@ -22,14 +18,17 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/member-delimiter-style': ['error', {
-      multiline: {
-        delimiter: 'none',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
       },
-      singleline: {
-        delimiter: 'semi',
-        requireLast: false
-      }
-    }]
+    ],
   },
-};
+}
