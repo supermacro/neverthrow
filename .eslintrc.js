bd3271dcc8771b432944c33b0a301d9cc5ea9351
Matt Pocock <mattpocockvoice@gmail.com>
2024-09-06 20:52:47 +0100

    Removed eslint-plugin-prettier and eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
   ],
   rules: {
     semi: 'off',
