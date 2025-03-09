b69e27e9dba139eac2891a5eccdd444e41adb00f
m-shaka <buddha0818@gmail.com>
2025-03-09 11:21:36 +0900

    fix tsconfig name

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     typecheck: {
       enabled: true,
       include: ['tests/typecheck-tests.ts'],
-      tsconfig: 'tests/tsconfig.test.json',
+      tsconfig: 'tests/tsconfig.tests.json',
     },
   },
 })
